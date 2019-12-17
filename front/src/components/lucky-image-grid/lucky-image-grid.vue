6425a6de4cf51639b8666abcf3fabba88613e3c0
zayfen <zhangyunfeng0101@gmail.com>
2019-12-17 23:01:01 +0800

    chore: commit code

---
+++ front/src/components/lucky-image-grid/lucky-image-grid.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="lucky-image-grid">
+    <div class="grid">
+      <div class="grid-item" v-for="(img, index) in images" :key="index">
+        <div class="grid-item__img" :style="{backgroundImage: 'url(' + img.src + ')'}"></div>
+        <div class="grid-item__container">
+          <h2>{{img.name}}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LuckyImageGrid",
+  props: {
+    images: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  },
+
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+.lucky-image-bed {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.grid {
+  display: flex;
+  max-width: 100%;
+  height: auto;
+  margin: 0 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: start;
+  align-items: flex-start;
+
+  .grid-item {
+    position: relative;
+    width: 100px;
+    height: 100px;
+    margin: 5px;
+
+    &__img {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+
+      display: block;
+      margin: 0;
+      line-height: 0;
+      max-width: 100px;
+      height: auto;
+      background-size: contain;
+      clip-path: circle(50% at center center);
+
+      &::before {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        content: '';
+        opacity: 0;
+        transition: opacity 0.5s;
+        background: rgba(purple, .25);
+      }
+
+      &::after {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        content: '';
+        opacity: 0;
+        transition: opacity 0.5s;
+        background: linear-gradient(to top, transparent, rgba(black, 0.5), transparent);
+
+      }
+    }
+
+    &__container {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%) scale(.9);
+      width: 100%;
+      
+      font-weight: 400;
+      font-size: 10px;
+      color: #fff;
+      text-align: center;
+
+      opacity: 0;
+      will-change: transform;
+      backface-visibility: hidden;
+      transition: all .5s;
+
+      h2 {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      }
+    }
+
+    &:hover {
+      .grid-item__container {
+        opacity: 1;
+        transform: translate(-50%, -50%) scale(1);
+      }
+
+      .grid-item__img {
+        &::before {
+          opacity: 1;
+        }
+        &::after {
+          opacity: 1;
+        }
+      }
+    }
+  } 
+}
+</style>
--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -1,11 +1,104 @@
 <template>
   <div class="luckydraw">
+    <el-row>
+      <h2 class="title">2019年瓶子科技年会抽奖</h2>
+    </el-row>
+    <el-row>
+      <el-col :span="6" class="luckydraw-participant">
+        <h4 class="luckydraw-participant__title">抽奖参与者({{participantList.length}}人)</h4>
+        <lucky-image-grid :images="participantList" class="luckydraw-participant__wrapper"></lucky-image-grid>
+      </el-col>
+      <el-col :span="12">
+        <div class="luckydraw-main">
+          <el-button size="large" :type="running ? 'warning' : 'primary'" class="luckydraw-main__button" @click="onButtonClick">{{running ? '产生幸运儿...' : '开始抽奖'}}</el-button>
+          <div class="luckydraw-main__image">
+            <h3 v-show="randomIndex > -1" style="color: red;">{{ participantList[randomIndex] && participantList[randomIndex].name}}</h3>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="luckydraw-forbidden">
+          <h4 class="luckydraw-forbidden__title">禁止参与({{forbiddenList.length}}人)</h4>
+          <lucky-image-grid :images="forbiddenList" class="luckydraw-forbidden__wrapper"></lucky-image-grid>
+        </div>
+      </el-col>
+    </el-row>
   </div>  
 </template>
+
 <script>
+import LuckyImageGrid from '@/components/lucky-image-grid/lucky-image-grid'
 export default {
+  components: {
+    LuckyImageGrid
+  },
   data () {
-    return {}
+    return {
+      participantList: [{
+          name: "张云峰",
+          src:
+            "https://res.cloudinary.com/zayfen/image/upload/v1576564059/img/wcylb7hjawi32w8ip72l.jpg"
+        },
+        {
+          name: "def",
+          src:
+            "https://res.cloudinary.com/zayfen/image/upload/v1576564059/img/mbzfmu8fbbtt9vtfsrrk.jpg"
+        },
+        {
+          name: "xxx",
+          src:
+            "https://res.cloudinary.com/zayfen/image/upload/v1576564059/img/prifpr8dchbxcvdkt6w9.jpg"
+        },
+        {
+          name: "sss",
+          src:
+            "https://res.cloudinary.com/zayfen/image/upload/v1576564059/img/idzscqkbuxccetwrzraj.jpg"
+        }],
+      forbiddenList: [
+        {
+          name: "zzz",
+          src:
+            "https://res.cloudinary.com/zayfen/image/upload/v1576564059/img/ch1mmihskxrt3wia7v6j.jpg"
+        }],
+      running: false,
+      randomIndex: -1,
+    }
+  },
+
+  methods: {
+    onButtonClick () {
+      if (this.running) {
+        clearInterval(this.intervalHolder)
+        setTimeout(() => {
+          this.showResult()
+        }, 0)
+
+      } else {
+        this.startDraw()
+      }
+      this.running = !this.running
+    },
+
+    showResult () {
+
+    },
+
+    startDraw () {
+      requestAnimationFrame((time) => {
+        if (!this.running) {
+          return 
+        }
+        this.randomIndex = this.generateRandomIndex()
+        console.log("randomIndex: ", this.randomIndex)
+        this.startDraw()
+      })
+    },
+
+    generateRandomIndex () {
+      let length = Math.max(1, this.participantList.length)
+      let number = Math.ceil(Math.random() * Math.pow(10, ('' + length).length))
+      return number % length
+    }
   }
 }
 </script>
@@ -17,5 +110,67 @@ export default {
   top: 0;
   right: 0;
   bottom: 0;
+
+  background: linear-gradient(#373b44, #4286f4);
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 30px;
+
+  .title {
+    text-align: center;
+    color: red;
+    margin-bottom: 0;
+    padding: 0;
+  }
+}
+
+.luckydraw-participant {
+  position: relative;
+  height: 820px;
+  overflow: hidden;
+  box-sizing: border-box;
+  border-top: 5px solid gray;
+  border-right: 5px solid gray;
+
+
+  &__title {
+    text-align: center;
+    color: #ccc;
+  }
+  &__wrapper {
+    overflow: auto;
+    height: 800px;
+
+  }
+}
+
+.luckydraw-main {
+  position: relative;
+  height: 820px;
+  overflow: hidden;
+  text-align: center;
+
+  &__button {
+    width: 330px;
+    margin-top: 30px;
+    font-size: 18px;
+  }
+}
+
+.luckydraw-forbidden {
+  position: relative;
+  height: 820px;
+  overflow: hidden;
+  box-sizing: border-box;
+  border-left: 5px solid gray;
+  border-top: 5px solid gray;
+
+  &__title {
+    text-align: center;
+    color: #ccc;
+  }
+  &__wrapper {
+    overflow: auto;
+    height: 800px;
+  }
 }
 </style>
