5c664d4b55abf5389282cbb39c86f207c5643e71
zayfen <zhangyunfeng0101@gmail.com>
2019-12-18 19:23:02 +0800

    commit code

--- front/src/pages/register/register.vue
+++ front/src/pages/register/register.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="register">
     <p class="title">瓶子科技2019年会抽奖</p>
-    <el-row>
-      <van-uploader :after-read="afterRead"></van-uploader>
-    </el-row>
-    <el-row class="form">
-      <el-input placeholder="请输入姓名"></el-input>
-    </el-row>
-    <el-row class="submit-button">
-      <el-button type="primary" :disabled="FormDisabled" @click="register">参与抽奖</el-button>
-    </el-row>
+
+    <van-row type="flex" justify="center">
+      <van-col :span="8">
+        <van-uploader :after-read="afterRead">
+          <van-button plain type="primary">上传图片</van-button>
+        </van-uploader>       
+      </van-col>
+      <van-col :span="8">
+        <van-button plain type="default" style="width: 4.4rem">随机图片</van-button>
+      </van-col>
+    </van-row>
+    <van-row style="margin-top: 10px;">
+      <van-image :src="avatar" width="6rem" height="6rem" :round="true" fit="cover">
+      </van-image>
+    </van-row>
+    <van-row class="form">
+      <van-field v-model="userName" label="用户名" placeholder="请输入姓名" />
+    </van-row>
+    <van-row class="submit-button">
+      <van-button type="primary" :disabled="FormDisabled" @click="register">参与抽奖</van-button>
+    </van-row>
   </div>
 </template>
 
@@ -36,6 +48,7 @@ export default {
 
     afterRead (file) {
       console.log("file: ", file)
+      
     }
   }
 }
@@ -49,9 +62,10 @@ export default {
   bottom: 0;
   left: 0;
   text-align: center;
+  background-color: rgba(0, 0, 0, .2);
 
   .title {
-
+    font-weight: 600;
   }
 
   .form {
